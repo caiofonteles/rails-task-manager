a7b17b3d50d73465a20e91f232a7a692a02f64d1
caio fonteles <fontelescaio@gmail.com>
2020-04-18 16:52:05 +0000

    add CRUD

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,5 +4,42 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def show
+    @task = Task.find(params[:id])
+  end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.new(params[:id])
+    @task.update(params[task_params])
+
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    # no need for app/views/tasks/destroy.html.erb
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # CREATE
+  get 'tasks/new', to: 'tasks#new', as: :new_restaurants
+  post 'tasks', to: 'tasks#create'
+
   # READ
   get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # UPDATE
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  update 'tasks/:id', to: 'tasks#uptade'
+
+  # DELETE
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
